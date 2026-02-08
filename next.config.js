32ee58191d20658b2871a24c8438c8c1da065f65
colinriche <colinriche@gmail.com>
2026-02-08 00:52:39 +0000

    Mixed Content: The page at '<URL>' was loaded over HTTPS, but attempted to connect to the insecure WebSocket endpoint 'ws:<URL>/'. This request has been blocked; this endpoint must be available over WSS.

--- next.config.js
+++ next.config.js
@@ -4,6 +4,20 @@ const nextConfig = {
   images: {
     domains: ['gomainstream.org'],
   },
+  // Upgrade insecure requests (including WebSocket) to secure when page is HTTPS
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "upgrade-insecure-requests",
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
